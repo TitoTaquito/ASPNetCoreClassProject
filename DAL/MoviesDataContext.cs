c69da656f56f48636c1d8370cd67cbf20d61c14a
Charles Spencer <Cspenc2060@gmail.com>
2019-05-10 10:15:22 -0700

    InitalCommit Added the Data Access Layer(DAL) and the home contloller

---
+++ DAL/MoviesDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    class MoviesDataContext: DbContext
+    {
+        public MoviesDataContext (DbContextOptions options):base(options) { }
+
+        public DbSet<Movie> Movies
+    }
+}
